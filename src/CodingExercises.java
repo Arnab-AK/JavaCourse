36d3238e33e771caaee97c7ee4c502590a32d194
Arnab-AK <royjason715@gmail.com>
2024-03-21 22:22:03 +0530

    Coding Exercise 8

--- src/CodingExercises.java
+++ src/CodingExercises.java
@@ -70,4 +70,62 @@ public class CodingExercises {
         }
     }
 
+    // Coding Exercise 5
+    public class LeapYear {
+        // write code here
+        public static boolean isLeapYear(int year) {
+            if (year >= 1 && year <= 9999) {
+                if ((year % 4) == 0) {
+                    if ((year % 100) == 0) {
+                        if ((year % 400) == 0) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Coding Exercise 6
+    public class DecimalComparator {
+        // write code here
+        public static boolean areEqualByThreeDecimalPlaces (double a, double b) {
+            int roundedNum1 = (int) (a * 1000);
+            int roundedNum2 = (int) (b * 1000);
+
+            return roundedNum1 == roundedNum2;
+        }
+    }
+
+    // Coding Exercise 7
+    public class EqualSumChecker {
+        // write code here
+        public static boolean hasEqualSum (int a, int b, int c) {
+            if ((a + b) == c ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Coding Exercise 8
+    public class TeenNumberChecker {
+        // write code here
+        public static boolean hasTeen (int a, int b, int c) {
+            return isTeen(a) || isTeen(b) || isTeen(c);
+        }
+
+        public static boolean isTeen(int number) {
+            return number > 12 && number < 20;
+        }
+    }
 }
