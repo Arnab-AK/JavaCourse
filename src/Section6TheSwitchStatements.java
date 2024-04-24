40b499b6625ad1ca395ad128c556fe44f09e9b22
Arnab-AK <royjason715@gmail.com>
2024-04-24 21:57:55 +0530

    Section 6 The Switch Statements

---
+++ src/Section6TheSwitchStatements.java
@@ -0,0 +1,30 @@
+public class Section6TheSwitchStatements {
+    public static void main(String[] args) {
+
+//        int value = 4;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 4;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("was a 3, a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1, 2, 3, 4, or 5");
+        }
+    }
+}
