d46acf6f42ac7ba779fe85ef8372ca848e5cc4a6
Arnab-AK <royjason715@gmail.com>
2024-06-11 22:24:37 +0530

    Section 6 Switch Statement Challenge

--- src/Section6TheSwitchStatements.java
+++ src/Section6TheSwitchStatements.java
@@ -12,19 +12,44 @@ public class Section6TheSwitchStatements {
 
         int switchValue = 4;
 
+//        switch (switchValue) {
+//            case 1:
+//                System.out.println("Value was 1");
+//                break;
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//            case 3: case 4: case 5:
+//                System.out.println("was a 3, a 4, or a 5");
+//                System.out.println("Actually it was a " + switchValue);
+//                break;
+//            default:
+//                System.out.println("Was not 1, 2, 3, 4, or 5");
+//        }
+
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
-                System.out.println("was a 3, a 4, or a 5");
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("It was a 3, a 4, or a 5");
                 System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Was not 1, 2, 3, 4, or 5");
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
+        String month = "OCTOBE";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter.");
+    }
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "February", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
     }
 }
